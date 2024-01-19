89f4bfbbbae6f44c0304027b14252417467aa848
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-19 22:25:32 +0600

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+
+    //upper part
+    for(row=n; row>=1; row--){
+        for(col=n-1; col>=row; col--)
+            printf(" ");
+        for(col=1; col<=row; col++){
+            printf("*",col);
+        }
+        printf("\n");
+    }
+
+
+    
+}
