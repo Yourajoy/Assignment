2115db2a2fe3dafa65a327123ceb74fc528e1148
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-19 22:31:02 +0600

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+
+    //upper part
+    for(row=n; row>=1; row--){
+        for(col=n-1; col>=row; col--) printf(" ");
+        for(col=1; col<row; col++){
+            printf("* ",col);
+        }
+        printf("*\n");
+    }
+    //lower part
+    for(row=1; row<=n; row++){
+        for(col=n-1; col>=row; col--) printf(" ");
+        for(col=1; col<row; col++){
+            printf("* ",col);
+        }
+        printf("*\n");
+    }
+    
+}
