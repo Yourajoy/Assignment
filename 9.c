b973a685417820632b55a94da661f8b099d6aec8
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-19 22:27:48 +0600

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+
+    //upper part
+    for(row=n; row>=1; row--){
+        for(col=n-1; col>=row; col--) printf(" ");
+        for(col=1; col<=(row+row-1); col++){
+            printf("*",col);
+        }
+        printf("\n");
+    }
+
+
+    
+}
