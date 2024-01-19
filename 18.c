883b91106fbc53f020c6b6bba01ac135792ae109
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-19 22:43:31 +0600

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+    int n,row,col,k=0;
+    scanf("%d",&n);
+    //upper part
+        for(row=2; row<=n; row++){
+
+        if(row!=1){
+            for(col=n; col>=row; col--) printf("*");
+        }
+
+        for(col=1; col<=k; col++){
+            printf(" ",col);
+        }
+
+        if(row!=1){
+            for(col=n; col>=row; col--) printf("*");
+        }
+
+        k+=2;
+        printf("\n");
+    }
+
+        //lower part
+        k-=2;
+        for(row=n; row>=2; row--){
+
+        for(col=n; col>=row; col--) printf("*");
+
+        for(col=1; col<=k; col++){
+            printf(" ",col);
+        }
+
+        for(col=n; col>=row; col--) printf("*");
+
+        k-=2;
+        printf("\n");
+    }
+
+    
+}
