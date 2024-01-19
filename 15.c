664cfc79c0408ce892039badf87bbd25d5631eea
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-19 22:38:55 +0600

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+
+    //upper part
+    for(row=1; row<=n; row++){
+
+        for(col=n-1; col>=row; col--) printf(" ");
+
+        printf("*");
+        for(col=1; col<row+row-2; col++){
+        printf(" ");
+        }
+
+        if(row!=1) printf("*");
+        printf("\n");
+
+    }
+
+
+    //lower part
+    for(row=n-1; row>=1; row--){
+
+        for(col=n-1; col>=row; col--) printf(" ");
+
+        printf("*");
+        for(col=1; col<row+row-2; col++){
+        printf(" ");
+        }
+
+        if(row!=1) printf("*");
+        printf("\n");
+
+    }
+    
+}
