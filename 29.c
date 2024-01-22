38b549c1d458ba255637efb61fb5e8010e833d26
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:19:03 +0600

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+    int n,row,col,k=0;
+    scanf("%d",&n);
+
+    k = n*2-2;
+    for(row=n; row>=1; row--){
+
+        for(col=n; col>=row; col--) printf("*");
+
+        for(col=1; col<=k; col++){
+            printf(" ",col);
+        }
+
+        for(col=n; col>=row; col--) printf("*");
+
+        k-=2;
+        printf("\n");
+    }
+
+        k+=2;
+        for(row=1; row<=n; row++){
+            if(row==1){
+                k+=2;
+                continue;
+            }
+
+        for(col=n; col>=row; col--) printf("*");
+
+        for(col=1; col<=k; col++){
+            printf(" ",col);
+        }
+
+        for(col=n; col>=row; col--) printf("*");
+
+        k+=2;
+        printf("\n");
+    }
+
+}
