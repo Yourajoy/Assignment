1292b9a702b4ff1b136076ed490a892d8af9b54c
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:30:51 +0600

    Update 10.c

--- 10.c
+++ 10.c
@@ -3,7 +3,6 @@ int main(){
     int n,row,col;
     scanf("%d",&n);
 
-    //upper part
     for(row=1; row<=n; row++){
         for(col=n-1; col>=row; col--) printf(" ");
         for(col=1; col<row; col++){
