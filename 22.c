822f3c0432b5c1926e1e8b58ac0024bcb19157be
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:04:35 +0600

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,row,col,k=0;
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row++){
+        for(col=1; col<row; col++) {
+                k++;
+                printf("%d ",k%2);
+        }
+        printf("%d\n",(++k)%2);
+    }
+
+
+    
+}
