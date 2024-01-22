88e25accc1a77da6f1ea44a07932ffbc961bcd58
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:03:08 +0600

    Update 8.c

--- 8.c
+++ 8.c
@@ -2,8 +2,6 @@
 int main(){
     int n,row,col;
     scanf("%d",&n);
-
-    //upper part
     for(row=1; row<=n; row++){
         for(col=n-1; col>=row; col--) printf(" ");
         for(col=1; col<=(row+row-1); col++){
