0c9db2b0e0c5eaccebed3cde2657ee50c7f973d8
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 21:59:17 +0600

    Update 15.c

--- 15.c
+++ 15.c
@@ -2,8 +2,6 @@
 int main(){
     int n,row,col;
     scanf("%d",&n);
-
-    //upper part
     for(row=1; row<=n; row++){
 
         for(col=n-1; col>=row; col--) printf(" ");
@@ -17,9 +15,6 @@ int main(){
         printf("\n");
 
     }
-
-
-    //lower part
     for(row=n-1; row>=1; row--){
 
         for(col=n-1; col>=row; col--) printf(" ");
