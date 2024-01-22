e463f53a84cc09070b7eb863cf74ea37a21ad353
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:23:24 +0600

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,row,col,k=0;
+    scanf("%d",&n);
+
+    for(row=n; row>=1; row--){
+    
+        for(col=row; col<n; col++) {
+                printf("%c ",col+64);
+        }
+        printf("%c\n",col+64);
+    }
+
+}
