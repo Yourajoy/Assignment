00c0a4f1dbf943ebdfdd4de13b0d9bddeaaa17d5
Ajoy Chakraborty <154905667+Yourajoy@users.noreply.github.com>
2024-01-22 22:20:37 +0600

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row++){
+
+        for(col=n-1; col>=row; col--) printf("  ");
+
+        for(col=row; col>=1; col--){
+        printf("%d ",col);
+        }
+
+        for(col=2; col<=row; col++){
+        printf("%d ",col);
+        }
+
+        printf("\n");
+
+    }
+
+
+}
